a7853d63847890a8fab796cbfc109b167c43620b
K140024(notebook)" <K140024@kdb.co.kr">
2024-03-26 19:52:11 +0900

    페이지네이션 수정 및 테스트 등

--- src/test/java/com/kdb/EPamsApplicationTests.java
+++ src/test/java/com/kdb/EPamsApplicationTests.java
@@ -1,11 +1,31 @@
 package com.kdb;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kdb.Board.BoardDTO;
+import com.kdb.Board.BoardService;
 
 @SpringBootTest
 class EPamsApplicationTests {
 
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void boardWrite() throws IOException {
+        for(int i=1; i<=100; i++) {
+            BoardDTO dto = new BoardDTO();
+            dto.setBoardTitle("제목"+i);
+            dto.setBoardContents("내용"+i);
+            dto.setBoardWriter("K140024");
+            boardService.save(dto);
+        }
+    }
+    
 	@Test
 	void contextLoads() {
 	}

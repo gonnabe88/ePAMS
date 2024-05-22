e53c798c198edd1f2a6ef28e22c06ba45ecf7ab6
K140024(notebook)" <K140024@kdb.co.kr">
2024-05-22 21:08:57 +0900

    webauth test

--- src/main/java/com/kdb/common/controller/MainController.java
+++ src/main/java/com/kdb/common/controller/MainController.java
@@ -83,6 +83,8 @@ public class MainController<S extends Session> {
 	      model.addAttribute("memberList", memberList);	      
 	      
     	return "/common/memberlist";
-    }     
+    }    
+
+    
 
 }

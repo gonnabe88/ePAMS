c540d1582f064cee641e8f862e22299b2e47ef2f
K140024(DESKTOP) <K140024@kdb.co.kr>
2024-07-30 23:46:27 +0900

    빌드쉘 수정

--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ record_time() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> ~/cron.log
 }
 
-# 환경 변수 설정
+# [삭제] 환경 변수 설정 (cron으로 실행할 경우에만 필요)
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-17.0.11.0.9-2.el8.x86_64
 export PATH=$JAVA_HOME/bin:$PATH
 export GRADLE_USER_HOME=$(pwd)/.gradle
 
-# Git 원격 저장소에서 최신 변경 사항 가져오기
+# [삭제] Git 원격 저장소에서 최신 변경 사항 가져오기 (Git을 사용하는 외부 운영서버에서만 필요)
 record_time "Git 원격 저장소에서 최신 변경 사항을 가져오는 중..."
 /usr/bin/git fetch origin
 /usr/bin/git reset --hard origin/main  # 필요한 경우 'main'을 실제 사용 중인 브랜치 이름으로 변경
 
-# gradlew와 build.sh 파일에 실행 권한 부여
+# [삭제] gradlew와 build.sh 파일에 실행 권한 부여 (Git을 사용하는 외부 운영서버에서만 필요)
 record_time "gradlew와 build.sh 파일에 실행 권한을 부여하는 중..."
 chmod +x ./gradlew
 chmod +x ./build.sh
@@ -39,19 +39,19 @@ record_time "프로젝트를 빌드하는 중..."
 if [ $? -eq 0 ]; then
     record_time "빌드 성공"
 
-    # WAR 파일 위치 (build/libs/ 디렉토리에 생성된 WAR 파일 가정)
-    WAR_FILE=$(find build/libs -name "*.war" | head -n 1)
+    # [삭제] WAR 파일 위치 (내부에서는 eCams가 수행)
+    WAR_FILE=$(find build/libs -name "*-plain.war" | head -n 1)
     if [ -z "$WAR_FILE" ]; then
         record_time "WAR 파일을 찾을 수 없습니다. 빌드가 성공했는지 확인하세요."
         exit 1
     fi
 
-    # Tomcat 웹 애플리케이션 디렉토리로 WAR 파일 복사
+    # [삭제] Tomcat 웹 애플리케이션 디렉토리로 WAR 파일 복사 (내부에서는 eCams가 수행)
     TOMCAT_WEBAPPS_DIR="/opt/tomcat/webapps"
     record_time "Tomcat 웹 애플리케이션 디렉토리로 WAR 파일을 복사하는 중..."
     cp "$WAR_FILE" "$TOMCAT_WEBAPPS_DIR/epams.war"
 
-    # Tomcat 서비스 재시작
+    # [삭제] Tomcat 서비스 재시작 (내부에서는 eCams가 수행)
     record_time "Tomcat 서비스를 재시작하는 중..."
     sudo systemctl restart tomcat
 

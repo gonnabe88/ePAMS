a330c21537738a2fe4fc9ded9f2a0562ce164f0a
K140024(DESKTOP) <K140024@kdb.co.kr>
2024-07-30 23:12:30 +0900

    빌드쉘 수정

--- build.sh
+++ build.sh
@@ -5,25 +5,35 @@ cd "$(dirname "$0")"
 
 # 시간 기록 함수
 record_time() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> ~/cron.log
 }
+
+# 환경 변수 설정
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-17.0.11.0.9-2.el8.x86_64
+export PATH=$JAVA_HOME/bin:$PATH
+export GRADLE_USER_HOME=$(pwd)/.gradle
 
 # Git 원격 저장소에서 최신 변경 사항 가져오기
 record_time "Git 원격 저장소에서 최신 변경 사항을 가져오는 중..."
-git fetch origin
-git pull origin main  # 필요한 경우 'main'을 실제 사용 중인 브랜치 이름으로 변경
+/usr/bin/git fetch origin
+/usr/bin/git reset --hard origin/main  # 필요한 경우 'main'을 실제 사용 중인 브랜치 이름으로 변경
+
+# gradlew와 build.sh 파일에 실행 권한 부여
+record_time "gradlew와 build.sh 파일에 실행 권한을 부여하는 중..."
+chmod +x ./gradlew
+chmod +x ./build.sh
 
 # Gradle 버전 확인
 record_time "Gradle 버전 확인 중..."
-./gradlew --version
+./gradlew --version >> ~/cron.log 2>&1
 
 # 기존 빌드 디렉토리 삭제
 record_time "기존 빌드 디렉토리 삭제 중..."
-./gradlew clean
+./gradlew clean >> ~/cron.log 2>&1
 
 # 빌드
 record_time "프로젝트를 빌드하는 중..."
-./gradlew build --offline
+./gradlew build --offline >> ~/cron.log 2>&1
 
 # 결과 확인
 if [ $? -eq 0 ]; then

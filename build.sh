716c7066109775ff4c7d0cd8f4674be98b0ea292
K140024(DESKTOP) <K140024@kdb.co.kr>
2024-08-23 00:09:42 +0900

    실환경 테스트

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-BUILD_LOG_FILE="$HOME/cron.log"
-WAS_LOG_FILE="$HOME/app.log"
+BUILD_LOG_FILE="/log/build_shell.log"
 
 # 프로젝트 루트 디렉토리로 이동
 cd "$(dirname "$0")"
@@ -11,17 +10,17 @@ record_time() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$BUILD_LOG_FILE"
 }
 
-# 환경 변수 설정 (cron으로 실행할 경우에만 필요)
+# 환경 변수 설정
 export JAVA_HOME=/sw/ehr/java-17-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 export GRADLE_USER_HOME=$(pwd)/.gradle
 
-# Git 원격 저장소에서 최신 변경 사항 가져오기 (Git을 사용하는 외부 운영서버에서만 필요)
+# [삭제] Git 원격 저장소에서 최신 변경 사항 가져오기 (Git을 사용하는 외부 운영서버에서만 필요)
 record_time "Git 원격 저장소에서 최신 변경 사항을 가져오는 중..."
 /usr/bin/git fetch origin
 /usr/bin/git reset --hard origin/main  # 필요한 경우 'main'을 실제 사용 중인 브랜치 이름으로 변경
 
-# gradlew와 build.sh 파일에 실행 권한 부여 (Git을 사용하는 외부 운영서버에서만 필요)
+# [삭제] gradlew와 build.sh 파일에 실행 권한 부여 (Git을 사용하는 외부 운영서버에서만 필요)
 record_time "gradlew와 build.sh 파일에 실행 권한을 부여하는 중..."
 chmod +x ./gradlew
 chmod +x ./build.sh
@@ -42,18 +41,18 @@ record_time "프로젝트를 빌드하는 중..."
 if [ $? -eq 0 ]; then
     record_time "빌드 성공"
 
-    # 기존 프로세스 종료
+    # [삭제] 기존 프로세스 종료
     record_time "기존 ePAMS 프로세스 종료 중..."
     pkill -f 'ePAMS' || true  # 프로세스 종료 시 실패해도 계속 진행
 
-    # JAR 파일 위치
+    # [삭제] JAR 파일 위치
     JAR_FILE=$(find build/libs -name "*-RELEASE.jar" -o -name "*.jar" | head -n 1)
     if [ -z "$JAR_FILE" ]; then
         record_time "JAR 파일을 찾을 수 없습니다. 빌드가 성공했는지 확인하세요."
         exit 1
     fi
 
-    # JAR 파일 실행
+    # [삭제] JAR 파일 실행
     # Java 난수 생성 시 빠르고 효율적인 /dev/urandom 사용하도록 설정
     # JVM 초기 힙 메모리 크기 2GB
     # JVM 최대 힙 메모리 크기 4GB
@@ -66,11 +65,11 @@ if [ $? -eq 0 ]; then
            -Xmx2G \
            -XX:+UseG1GC \
            -XX:+UseStringDeduplication \
-           -jar "$JAR_FILE" > "$WAS_LOG_FILE" 2>&1 &
+           -jar "$JAR_FILE"
 
-    # 성공 메시지
+    # [삭제] 성공 메시지
     record_time "배포 및 실행 성공"
 else
-    record_time "빌드 또는 실행 중 오류 발생"
+    record_time "빌드 중 오류 발생"
     exit 1
 fi

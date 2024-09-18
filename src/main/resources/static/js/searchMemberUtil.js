addecdf36e3248e57b286625087ca8de3e5d1432
K140024(DESKTOP) <K140024@kdb.co.kr>
2024-09-18 20:42:04 +0900

    직원검색 터치 이벤트 사용감 개선

--- src/main/resources/static/js/searchMemberUtil.js
+++ src/main/resources/static/js/searchMemberUtil.js
@@ -208,13 +208,12 @@ $(document).ready(() => {
 
         // 스크롤이 내부에서만 발생하도록 설정
         event.stopPropagation();
-        console.log("[scroll] stopPropagation#1");
+        //console.log("[scroll] stopPropagation#1");
 
     }, { passive: false });
 
     // 터치 이벤트에서 스크롤이 외부로 전파되지 않도록 설정
     dropdown.addEventListener('touchmove', function(event) {
-        event.preventDefault();
         event.stopPropagation();
         console.log("[touchmove] stopPropagation#2");
     }, { passive: false });
